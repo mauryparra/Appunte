292f0418c98cdc5eb201c35e9d5b383be15afe31
Mauricio Parra Casado <mauryparra@gmail.com>
2014-05-12 17:40:39 -0300

    Nuevo Bundle Appunte

---
+++ src/prog3/AppunteBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace prog3\AppunteBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
---
+++ src/prog3/AppunteBundle/prog3AppunteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace prog3\AppunteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class prog3AppunteBundle extends Bundle
+{
+}
